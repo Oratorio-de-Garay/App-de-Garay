7c89b5eec0e1f928193b07c9d13ba7ebb5263756
Nicolas Altman <altman.nicolas44@gmail.com>
2026-05-23 01:07:10 -0300

    Version de Apps Scripts actualizada

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// const urlScript = "https://script.google.com/macros/s/AKfycbwiUjEWYIJGy3AR00wULyzsYCxKYarvpkkGC6bq3Abf_5zGgAtKIpzJl2JTP3_N3Lfr/exec";
-const urlScript = "https://script.google.com/macros/s/AKfycbwgnLTfIIeYawtIFVEtTbw01yU3orNIGCqeN0IUpdMG38X9eFb-_SetPl96nZbScbbA/exec";
+// PROD
+const urlScript = "https://script.google.com/macros/s/AKfycbwgnLTfIIeYawtIFVEtTbw01yU3orNIGCqeN0IUpdMG38X9eFb-_SetPl96nZbScbbA/exec"
 
 // DEV
 // const urlScript = "https://script.google.com/macros/s/AKfycbwHN4f04yaKT25T6JaJErlB_YtpKsHjUGmGOLiSFq0/dev";
